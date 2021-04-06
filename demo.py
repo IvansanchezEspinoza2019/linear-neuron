382a63539f2a966dcdf0deb9de62c8ce4982dfdb
IvansanchezEspinoza2019 <edgar.sanchez1492@alumnos.udg.mx>
2021-04-06 11:07:36 -0500

    Comment corrected

--- demo.py
+++ demo.py
@@ -24,7 +24,7 @@ plt.plot(xn.ravel(), neuron.predict(xn), '--k')
 #### read dataset #####
 dataset = pd.read_csv("DataSet1.csv")
 
-## Creating the a training and y training ###
+## creating the X,Y for training ###
 x_train = np.array([dataset.iloc[:, 0]])  # vector size: 1 x 99
 y_train = np.array([dataset.iloc[:, 1]])  # vector size: 1 x 99
 
@@ -32,7 +32,7 @@ y_train = np.array([dataset.iloc[:, 1]])  # vector size: 1 x 99
 linear_regression = LinearNeuron(1, 0.1) # 1 dimension problem, 0.1 neuron learning factor
 
 ### plot a dataset ###
-plt.title("LINEAR REGRESION")
+plt.title("LINEAR REGRESSION")
 print(x_train.shape)
 print(y_train.shape)
 
@@ -41,11 +41,11 @@ plt.scatter(x_train, y_train, label='Train Dataset')
 
 ### training ###
 # you can choose between 'SGD', 'BGD' and 'DIRECT' algorithms
-# receive X shape: NxP, Y shape: 1xP
-linear_regression.fit(x_train, y_train, solver='SGD', epochs=500) 
+# receive:   X: NxP;    Y: 1xP
+linear_regression.fit(x_train, y_train, solver='SGD', epochs=400) 
 
-### making the line regresion ###
-x_vector = np.array([[0, 1]])  # create points between 0 and 1, dimension: 1xP
+### making the line regression ###
+x_vector = np.array([[0, 1]])  # create points between 0 and 1
 y_vector = linear_regression.predict(x_vector)  # predict the x vector
 
 print(x_vector.shape)
@@ -54,7 +54,7 @@ print(y_vector.shape)
 ### plot lineal regression ###
 plt.plot(x_vector.ravel(), y_vector, '--k', label="Regression", linewidth=2)
 
-### making prediction ###
+### making predictions ###
 x_pred = np.array([[0.5, 0.2, 0.4, 0.87, 0.6, 0.75, 0.15]]) # new data for prediction
 y_pred = linear_regression.predict(x_pred)                  # predict new data
         
